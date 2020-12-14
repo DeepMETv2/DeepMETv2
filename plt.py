43b0a421fcadc319c9997d7a99c950343b4867a5
Matteo Cremonesi <matteo.cremonesi@cern.ch>
2020-12-15 02:50:22 +0900

    minor edits to plots

--- plt.py
+++ plt.py
@@ -40,23 +40,23 @@ for key in resolutions_arr:
          plt.figure(1)
          xx = a[key]['u_perp_resolution'][1][0:20]
          yy = a[key]['u_perp_resolution'][0]
-         plt.hist(xx, weights = yy, bins = xx,color=colors[key], label=label_arr[key],histtype='step',)
+         plt.plot(xx, yy,color=colors[key], label=label_arr[key])
          plt.figure(2)
          xx = a[key]['u_perp_scaled_resolution'][1][0:20]
          yy = a[key]['u_perp_scaled_resolution'][0]
-         plt.hist(xx, weights = yy, bins = xx,color=colors[key], label=key,histtype='step',)
+         plt.plot(xx, yy,color=colors[key], label=label_arr[key])
          plt.figure(3)
          xx = a[key]['u_par_resolution'][1][0:20]
          yy = a[key]['u_par_resolution'][0]
-         plt.hist(xx, weights = yy, bins = xx,color=colors[key], label=key,histtype='step',)
+         plt.plot(xx, yy,color=colors[key], label=label_arr[key])
          plt.figure(4)
          xx = a[key]['u_par_scaled_resolution'][1][0:20]
          yy = a[key]['u_par_scaled_resolution'][0]
-         plt.hist(xx, weights = yy, bins = xx,color=colors[key], label=key,histtype='step',)
+         plt.plot(xx, yy,color=colors[key], label=label_arr[key])
          plt.figure(5)
          xx = a[key]['R'][1][0:20]
          yy = a[key]['R'][0]
-         plt.hist(xx, weights = yy, bins = xx,color=colors[key], label=key,histtype='step',)
+         plt.plot(xx, yy,color=colors[key], label=label_arr[key])
 
 if(True):
     model_dir=args.ckpts+'/'+args.restore_file+'_'
